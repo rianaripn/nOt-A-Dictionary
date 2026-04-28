c1bebc68931e0942cc71969b669eeed24be12ee7
Rian Arip Nugraha <rianarip4@gmail.com>
2026-04-28 14:40:59 +0700

    fixbug:body-groq

--- api/groq.js
+++ api/groq.js
@@ -1,25 +1,36 @@
 const BASE_URL = "https://api.groq.com/openai/v1/chat/completions"
 
+const BASE_URL = "https://api.groq.com/openai/v1/chat/completions"
 export default async function handler(req, res) {
-    console.log('method:', req.method)
-    console.log('body:', req.body)
-    console.log('body type:', typeof req.body)
-    
-    // Parse body manual dari stream
-    let rawBody = ''
-    await new Promise((resolve) => {
-        req.on('data', chunk => rawBody += chunk)
-        req.on('end', resolve)
-    })
-    
-    console.log('rawBody:', rawBody)
-    
-    return res.status(200).json({ 
-        debug: true, 
-        body: req.body, 
-        method: req.method,
-        rawBody: rawBody
-    })
+    const { word, definition } = req.body
+    const prompt = `
+    word = ${word}
+    definition = ${definition}
+    You are a witty, sarcastic wordsmith. Define this word sarcastically in 1-3 sentences. Output ONLY the sarcastic DEFINITION, nothing else. 
+    AND ADD AN EMOJI THAT RELATE TO THAT IN THE END OF THE SENTENCE. ONLY USE 30 WORD MAX. and i your output is ONLY follow this format : 
+    [word_type] | [sarcastic definition]
+    example : noun | Love is a temporary
+    `
+    try {
+        const response = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.GROQ_API_KEY}`
+            },
+            body: JSON.stringify({
+                model: 'llama-3.1-8b-instant',
+                messages: [{ role: 'user', content: prompt }]
+            })
+        })
+        if (!response.ok) {
+            throw new Error('The AI refused to speak to you. Probably for the best🙄.')
+        }
+        const result = await response.json()
+        return res.status(200).json({ aiResponse: result.choices[0].message.content })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 }
 
 
